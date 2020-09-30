369945bdb9e78955d35a4addf7894344bf4d93fb
Georg Sachs <gsax@protaisn.org>
2020-09-30 22:54:17 +0200

    Refactor

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,15 +1,22 @@
 package main
 
-import "errors"
+const (
+	// ErrNotFound means the definition could not be found for the given word.
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+
+	// ErrWordExists means you are trying to add a word that is already known
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+// DictionaryErr are errors that can happen when interacting with the dictionary.
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 // Dictionary store definitions to words.
 type Dictionary map[string]string
-
-// ErrNotFound means the definition could not be found for the given word.
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
-)
 
 // Search find a word in the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
